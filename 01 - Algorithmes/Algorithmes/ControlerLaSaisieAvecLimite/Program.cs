2fd09dd456d6368e754224f2512266d19f025494
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-09-24 15:51:12 +0200

    mise en place de l'exercice "controle de la saisie avec limite"

--- 01 - Algorithmes/Algorithmes/ControlerLaSaisieAvecLimite/Program.cs
+++ 01 - Algorithmes/Algorithmes/ControlerLaSaisieAvecLimite/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* L’utilisateur est invité à saisir un mot de passe.
+
+Si le mot de passe saisi est correct, le programme affiche “Vous êtes connecté”.
+
+Dans le cas contraire, l’utilisateur doit recommencer la saisie.
+
+L’utilisateur dispose de 3 essais maximum.
+
+Au 3ème échec, le programme affiche “Votre compte est bloqué” et se termine.
+
+Note : Le bon mot de passe est formation */
+
+// VARIABLES
+
+
+
+// TRAITEMENT
+
+Console.WriteLine("Exercice 3a.2 : Contrôler la saisie avec limite");
+
+
+    
