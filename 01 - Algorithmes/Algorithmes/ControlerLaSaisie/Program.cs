ea6a52bbf8cf65c61da52d25ba2fe9792be382c5
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-09-24 14:46:16 +0200

    exercice "contrôle de saisie"

--- 01 - Algorithmes/Algorithmes/ControlerLaSaisie/Program.cs
+++ 01 - Algorithmes/Algorithmes/ControlerLaSaisie/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* L’utilisateur est invité à saisir son prénom.
+
+Le programme affiche ensuite “Bonjour” suivi du prénom saisi.
+
+Le prénom doit contenir au moins 2 caractères.
+
+Si le prénom contient moins de 2 caractères, l’utilisateur doit recommencer la saisie.
+
+L’utilisateur dispose d’un nombre d’essai illimité. */
+
+// VARIABLES
+
+string prenom;
+
+// TRAITEMENT
+
+Console.WriteLine("Exercice 3a.1 : Contrôler la saisie");
+
+do
+{
+    Console.WriteLine("Saisissez votre prénom :");
+    prenom = Console.ReadLine();
+}
+
+while (prenom.Length < 2);
+
+Console.WriteLine("Bonjour " + prenom);
