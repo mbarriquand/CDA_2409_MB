5110ce3d82d65194e3c42d8d3e2fd3407469a38a
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-09-25 10:14:56 +0200

    mise en place exercice "nombre premier" et "jeu de la fourchette"

---
+++ 01 - Algorithmes/Algorithmes/JeuDeLaFourchette/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Exercice 3a.6 : Jeu de la fourchette :");
+
+/*
+L’ordinateur « choisit » aléatoirement un nombre mystère (entier compris entre 0 et 100).
+Le joueur essaie de le deviner.
+Lors de chaque essai, l’ordinateur affiche la « fourchette » dans laquelle se trouve le nombre qu’il a choisi.
+
+Le choix du nombre mystère sera simulé par génération d’un nombre aléatoire : N <-- RANDOM(0,100).
+
+Lorsque l’utilisateur a trouvé le nombre mystère, le programme affiche :
+
+Bravo vous avez trouvé en X essais.
+*/
+
+// VARIABLES
+
+// TRAITEMENT
+
---
+++ 01 - Algorithmes/Algorithmes/NombrePremier/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Exercice 3a.5 : Nombre premier");
+
+/* Lire un nombre N et déterminer s’il est un nombre premier.
+
+Un nombre premier n’est divisible que par 1 et par lui-même. */
+
+// VARIABLES
+
+// TRAITEMENT
+
