9993161abd53611f7cb578ab5a63d198943d2c85
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-09-25 09:32:05 +0200

    fin exercice intervalle

---
+++ 01 - Algorithmes/Algorithmes/Intervalle/Program.cs
@@ -0,0 +1,32 @@
+﻿// Lire 2 nombres entier A et B puis afficher tous les nombres entier dans l’intervalle.
+
+Console.WriteLine("Exercice 3a.3 : Intervalle entre 2 nombres");
+
+// VARIABLES
+
+int A;
+int B;
+
+// TRAITEMENT
+
+Console.WriteLine("Saisissez un nombre A :");
+A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Saisissez un nombre B :");
+B = int.Parse(Console.ReadLine());
+
+if (A < B)
+{
+    for (int i = A++; i < B; i++)
+    {
+        Console.WriteLine("Résultat = " + i + " ");
+    }
+}
+
+else if (B < A)
+{
+    for (int i = A--; i > B; i--)
+    {
+        Console.WriteLine("Résultat = " + i + " ");
+    }
+}
