77072adc1e4d9bf2f440211f5ddec9ef93f1431a
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-09-25 11:15:58 +0200

    fin exercice "nombre premier"

--- 01 - Algorithmes/Algorithmes/NombrePremier/Program.cs
+++ 01 - Algorithmes/Algorithmes/NombrePremier/Program.cs
@@ -6,5 +6,31 @@ Un nombre premier n’est divisible que par 1 et par lui-même. */
 
 // VARIABLES
 
+int N;
+int check;
+check = 0;
+
 // TRAITEMENT
+
+Console.WriteLine("Saisissez un nombre :");
+N = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i <= N; i++)
+{
+    if (N % i == 0)
+    {
+        check++;
+    }
+}
+
+    if (check == 2)
+    {
+    Console.WriteLine(N + " est un nombre premier");
+    }
+
+    else
+    {
+        Console.WriteLine(N + " n'est pas un nombre premier");
+    }
 
