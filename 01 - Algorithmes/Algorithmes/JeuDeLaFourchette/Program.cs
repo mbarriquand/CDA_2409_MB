e9db17a201466ce3671043bfe74029ca191b7386
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-09-25 15:56:49 +0200

    début fourchette

--- 01 - Algorithmes/Algorithmes/JeuDeLaFourchette/Program.cs
+++ 01 - Algorithmes/Algorithmes/JeuDeLaFourchette/Program.cs
@@ -12,7 +12,36 @@ Lorsque l’utilisateur a trouvé le nombre mystère, le programme affiche :
 Bravo vous avez trouvé en X essais.
 */
 
+// exercice non terminé
+
 // VARIABLES
+
+Random random = new Random(); 
+int numRand = random.Next(0,100); // Génère un nombre random
+
+int userGuess = 0;
+int tries = 0;
+int min = 1;
+int max = 100;
 
 // TRAITEMENT
 
+while (userGuess != numRand)
+{
+    Console.WriteLine("Devinez le nombre mystère (entre 1 et 100) : ");
+    userGuess = int.Parse(Console.ReadLine());
+
+    if (userGuess > numRand)
+    {
+        Console.WriteLine("Le nombre mystère est plus petit");
+    }
+
+    else if (userGuess < numRand)
+    {
+        Console.WriteLine("Le nombre mystère est plus grand");
+    }
+        
+    tries++;
+
+}
+Console.WriteLine("Bravo vous avez trouvé en " + tries + " essais");
