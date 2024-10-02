e6a348b1e44e4bb14954dac3a73b847b035bf500
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-10-02 15:27:59 +0200

    exercice 'mot de passe sécurisé'

--- 01 - Algorithmes/Algorithmes/ControlerLaSaisieAvecLimite/Program.cs
+++ 01 - Algorithmes/Algorithmes/ControlerLaSaisieAvecLimite/Program.cs
@@ -1,32 +1,28 @@
-﻿/* L’utilisateur est invité à saisir un mot de passe.
+﻿/* CONSIGNES
+ * L’utilisateur est invité à saisir un mot de passe.
+ * Si le mot de passe saisi est correct, le programme affiche “Vous êtes connecté”.
+ * Dans le cas contraire, l’utilisateur doit recommencer la saisie.
+ * L’utilisateur dispose de 3 essais maximum.
+ * Au 3ème échec, le programme affiche “Votre compte est bloqué” et se termine.
+ * Note : Le bon mot de passe est formation */
 
-Si le mot de passe saisi est correct, le programme affiche “Vous êtes connecté”.
-
-Dans le cas contraire, l’utilisateur doit recommencer la saisie.
-
-L’utilisateur dispose de 3 essais maximum.
-
-Au 3ème échec, le programme affiche “Votre compte est bloqué” et se termine.
-
-Note : Le bon mot de passe est formation */
+Console.WriteLine("Exercice 3a.2 : Contrôler la saisie avec limite");
 
 // VARIABLES
 
-string mdp;
+const string BON_MOT_DE_PASSE = "formation";
+const int ESSAIS_MAX = 3;
+
 string saisieUtilisateur;
 
 // TRAITEMENT
 
-mdp = "formation";
-
-Console.WriteLine("Exercice 3a.2 : Contrôler la saisie avec limite");
-
-for (int i = 0; i < 3; i++)
+for (int i = 0; i < ESSAIS_MAX; i++)
 {
     Console.WriteLine("Saisissez votre mot de passe :");
     saisieUtilisateur = Console.ReadLine();
 
-    if (saisieUtilisateur == mdp)
+    if (saisieUtilisateur == BON_MOT_DE_PASSE)
     {
         Console.WriteLine("Vous êtes connecté");
         System.Environment.Exit(1);
@@ -42,3 +38,28 @@ for (int i = 0; i < 3; i++)
         Console.WriteLine("Mot de passe erroné");
     }
 }
+
+
+/* Autre possibilité avec une boucle while :
+  
+  const string BON_MOT_DE_PASSE = "formation";
+  const int ESSAIS_MAX = 3;
+
+  string saisieUtilisateur;
+  int compteur = 1;
+  
+  Console.WriteLine("Saisissez votre mot de passe :");
+        saisieUtilisateur = Console.ReadLine();
+   
+   while (!saisieUtilisateur.Equals(BON_MOT_DE_PASSE))
+  {
+      
+      Console.WriteLine($"Mot de passe erroné {compteur}/{ESSAIS_MAX}");
+       Console.WriteLine("Recommencez");
+       saisieUtilisateur = Console.ReadLine();
+       
+       compteur++;
+      
+  }
+    
+   */
