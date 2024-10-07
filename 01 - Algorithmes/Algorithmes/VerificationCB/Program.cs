bae4ba1ff3b06ccec40723e0b03857cb86b3c3f5
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-10-07 09:16:23 +0200

    création d'une classe "vérification de CB par regex"

---
+++ 01 - Algorithmes/Algorithmes/VerificationCB/Program.cs
@@ -0,0 +1,16 @@
+﻿using MBarriquandTools;
+
+namespace VerificationCB
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string saisieUtilisateur;
+
+            saisieUtilisateur = ConsoleTools.ComposantCBRegex("Verifiez le format CB :");
+
+        }
+    }
+}
