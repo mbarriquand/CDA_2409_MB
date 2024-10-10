81d53e83824d4396eedbe4a803d5b41617860d3e
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-10-10 11:10:03 +0200

    array vs. list, array version

---
+++ 01 - Algorithmes/Algorithmes/Collections.Listes/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Collections.Listes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* CONSIGNES :
+            Au démarrage, il n'y a aucun utilisateur enregistré.
+ 
+            1. Le programme demande à l'utilisateur de saisir un nom et un prénom.
+	            - L’utilisateur saisit un nom et un prénom.
+ 
+            2. Lorsque toutes les informations sont saisies
+	            - Le programme enregistre l'utilisateur
+ 
+            3. Le programme demande à l'utilisateur s'il souhaite ajouter une autre personne.
+	            - Si oui
+		            - Retour à l'étape 1 (saisir nom et prénom)
+	            - Si non
+		            - Afficher tous les utilisateurs enregistrés
+ 
+            4. Le programme se termine
+            */
+
+            // VARIABLES
+
+            string saisieNomPrenom;
+            char saisieOuiNon;
+
+            string[] utilisateurs;
+            utilisateurs = new string[] { "Mary Poppins" };
+
+            string[] tableauTemporaire;
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.WriteLine("\nSaisissez votre nom et prénom : ");
+                saisieNomPrenom = Console.ReadLine() ?? " ";
+
+                tableauTemporaire = utilisateurs;
+
+                utilisateurs = new string[utilisateurs.Length + 1];
+                tableauTemporaire.CopyTo(utilisateurs, 0);
+
+                utilisateurs[utilisateurs.Length-1] = saisieNomPrenom;
+
+                Console.WriteLine("\nSaisissez-vous ajouter un autre utilisateur ? (o/n)");
+                saisieOuiNon = Console.ReadKey().KeyChar;
+                
+            }
+            while (saisieOuiNon == 'o' || saisieOuiNon == 'O');
+
+
+        }
+    }
+}
