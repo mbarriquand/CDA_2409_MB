51141a601057fcdc0c696e2caf6235a48598d366
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2024-10-10 08:48:07 +0200

    correction post panique inutile du jalon #1

--- 01 - Algorithmes/Algorithmes/JalonAlgo/Program.cs
+++ 01 - Algorithmes/Algorithmes/JalonAlgo/Program.cs
@@ -9,13 +9,15 @@ namespace JalonAlgo
             Console.WriteLine("Jalon :");
 
             // VARIABLES
-            
-            string utilisateur;            
+
+            string utilisateur;
+            int solde = 0;
             int prixRepas = 4;
             bool utilisateurExistant = false;
+            bool soldeSuffisant = true;
 
             string[] tableauUtilisateur = { "WAFAE", "REMI", "KARAN", "ALLAN", "MARIE" };
-            int[] tableauSolde = {25, 10, 4, 2, 0};
+            int[] tableauSolde = { 25, 10, 4, 2, 0 };
 
             // TRAITEMENT
 
@@ -25,26 +27,43 @@ namespace JalonAlgo
 
             for (int i = 0; i < tableauUtilisateur.Length; i++)
             {
+
                 if (utilisateur == tableauUtilisateur[i])
                 {
                     utilisateurExistant = true;
 
                     if (tableauSolde[i] - prixRepas >= 0)
+
                     {
-                            tableauSolde[i] -= prixRepas;
-                            Console.WriteLine("Repas réglé.\t Nouveau solde : " + tableauSolde[i]);
+                        tableauSolde[i] -= prixRepas;
+                        solde = tableauSolde[i];
+                        soldeSuffisant = true;
                     }
 
                     else
                     {
-                           Console.WriteLine("Le solde de l'utilisateur " + utilisateur + " est insuffisant.");
+                        soldeSuffisant = false;
                     }
 
                 }
-            } 
+            }
 
-            if (!utilisateurExistant)
-            Console.WriteLine("L'utilisateur " + utilisateur + " n'existe pas dans la base.");
+            // AFFICHAGE
+
+            if (utilisateurExistant && soldeSuffisant)
+            {
+                Console.WriteLine("Repas réglé.\t Nouveau solde : " + solde);
+            }
+
+            else if (utilisateurExistant && !soldeSuffisant)
+            {
+                Console.WriteLine("Le solde de l'utilisateur " + utilisateur + " est insuffisant.");
+            }
+
+            else
+            {
+                Console.WriteLine("L'utilisateur " + utilisateur + " n'existe pas dans la base.");
+            }
         }
     }
 }
