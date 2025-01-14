66542c132d0444b10a9f8f3be530f04b27a36a2c
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-01-14 12:40:03 +0100

    tp fin

--- 03 - Conception BDD/tp-employes/employes_tp.sql
+++ 03 - Conception BDD/tp-employes/employes_tp.sql
@@ -1,3 +1,5 @@
+SET lc_time_names = 'fr_FR';
+
 -- 1. Donner nom, job, numéro et salaire de tous les employés, puis seulement des employés du département 10
 SELECT ename, job, empno, sal
 FROM emp
@@ -60,7 +62,8 @@ FROM emp
 WHERE job = 'SALESMAN';
 
 -- 12. Liste des employés du département 20: nom, job, date d'embauche sous forme VEN 28 FEV 1997'
-SELECT ename, job, DATE_FORMAT(hiredate, "%W %e %M %Y")
+
+SELECT ename, job, UPPER(DATE_FORMAT(hiredate, "%W %e %M %Y"))
 FROM emp
 WHERE deptno = 20;
 
@@ -122,6 +125,9 @@ UPDATE emp SET num_proj = 102
 WHERE num_proj IS NULL;
 
 -- 20. Créer une vue comportant tous les employés avec nom, job, nom de département et nom de projet
+
+DROP VIEW view_employe;
+
 CREATE VIEW view_employe AS
 SELECT ename, job, dname, name_proj
 FROM emp
@@ -135,12 +141,19 @@ SELECT ename, job, dname, name_proj
 FROM view_employe
 ORDER BY dname ASC, name_proj ASC;
 
--- 22.Donner le nom du projet associé à chaque manager
+-- 22. Donner le nom du projet associé à chaque manager
 
 SELECT ename, name_proj FROM emp
 JOIN project
 ON project.num_proj = emp.num_proj
 WHERE job = 'manager';
+
+/*
+en utilisant la vue
+SELECT ename, name_proj FROM emp
+FROM view_employe
+WHERE job = 'manager';
+ */
 
 -- Deuxième partie
 -- 1. Afficher la liste des managers des départements 20 et 30
@@ -172,21 +185,120 @@ WHERE loc = 'dallas';
 
 -- 6. Afficher les noms et dates d'embauche des employés embauchés avant leur manager, avec le nom et date d'embauche du manager.
 
-
+SELECT e.ename AS 'employes', m.ename AS 'managers', e.hiredate AS 'employes hiredate', m.hiredate AS 'managers hiredate'
+FROM emp e
+JOIN emp m 
+ON e.mgr = m.empno
+WHERE e.hiredate < m.hiredate;
 
 -- 7. Lister les numéros des employés n'ayant pas de subordonné.
 
-SELECT ename, job FROM emp
-WHERE mgr IS NULL;
+SELECT empno, ename
+FROM emp
+WHERE empno NOT IN (
+    SELECT DISTINCT mgr
+    FROM emp
+    WHERE mgr IS NOT NULL
+);
 
 -- 8. Afficher les noms et dates d'embauche des employés embauchés avant BLAKE.
 
-
+SELECT ename, hiredate
+FROM emp
+WHERE hiredate < (
+SELECT DISTINCT hiredate
+FROM emp
+WHERE ename = 'BLAKE');
 
 -- 9. Afficher les employés embauchés le même jour que FORD.
 
+SELECT ename, hiredate
+FROM emp
+WHERE hiredate = (
+SELECT DISTINCT hiredate
+FROM emp
+WHERE ename = 'FORD')
+AND ename != 'FORD';
+
 -- 10. Lister les employés ayant le même manager que CLARK.
+
+SELECT ename, mgr
+FROM emp
+WHERE mgr = (
+SELECT DISTINCT mgr
+FROM emp
+WHERE ename = 'CLARK')
+AND ename != 'CLARK';
 
 -- 11. Lister les employés ayant même job et même manager que TURNER.
 
+SELECT ename, job, mgr
+FROM emp
+WHERE mgr = (
+SELECT DISTINCT mgr
+FROM emp
+WHERE ename = 'TURNER')
+AND job = (
+SELECT DISTINCT job
+FROM emp
+WHERE ename = 'TURNER');
+
 -- 12. Lister les employés du département RESEARCH embauchés le même jour que quelqu'un du département SALES.
+
+SELECT er.empno, er.ename, er.hiredate
+FROM emp er
+JOIN emp es ON er.hiredate = es.hiredate
+JOIN dept dr ON er.deptno = dr.deptno
+JOIN dept ds ON es.deptno = ds.deptno
+WHERE dr.dname = 'RESEARCH'
+AND ds.dname = 'SALES';
+
+-- 13. Lister le nom des employés et également le nom du jour de la semaine correspondant à leur date d'embauche.
+
+SELECT ename AS 'nom de l\'employé',
+UPPER(DATE_FORMAT(hiredate,'%W')) AS 'jour d\'embauche'
+FROM emp;
+
+-- 14. Donner, pour chaque employé, le nombre de mois qui s'est écoulé entre leur date d'embauche et la date actuelle.
+
+SELECT ename AS 'nom de l\'employé',
+TIMESTAMPDIFF(MONTH, hiredate, CURDATE()) AS 'mois depuis l\'embauche'
+FROM emp;
+
+-- 15. Afficher la liste des employés ayant un M et un A dans leur nom.
+
+SELECT ename FROM emp
+WHERE ename LIKE '%M%' '%A%';
+
+-- 16. Afficher la liste des employés ayant deux 'A' dans leur nom.
+
+SELECT ename FROM emp
+WHERE ename LIKE '%A%A%';
+
+-- 17. Afficher les employés embauchés avant tous les employés du département 10.
+
+SELECT e10.ename, e10.hiredate
+FROM emp e10
+WHERE e10.hiredate < (
+    SELECT MIN(hiredate)
+    FROM emp
+    WHERE deptno = 10
+);
+
+-- 18. Sélectionner le métier où le salaire moyen est le plus faible.
+
+SELECT job, AVG(sal) AS 'salaire moyen'
+FROM emp
+GROUP BY job
+ORDER BY 'salaire moyen' ASC
+LIMIT 1;
+
+-- 19. Sélectionner le département ayant le plus d'employés.
+
+SELECT d.dname, COUNT(e.empno) AS 'nombre d\'employés'
+FROM dept d
+JOIN emp e
+ON e.deptno = d.deptno
+GROUP BY d.dname
+ORDER BY 'nombre d\'employés' DESC
+LIMIT 1;
