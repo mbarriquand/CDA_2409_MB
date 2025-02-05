1d924745ce8fc837776b8a6e675cdeee20b063bd
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-02-05 11:42:56 +0100

    bouteille (classe, constructeurs, début méthodes)

---
+++ 04 - UML, POO/ApplicationBouteille/ApplicationBouteille/Bouteille.cs
@@ -0,0 +1,76 @@
+﻿namespace ApplicationBouteille
+{
+    internal class Bouteille
+    {
+        // ATTRIBUTS
+
+        private float contenanceMaxEnL;
+        private float contenuEnL;
+        private bool estOuverte;
+
+        
+        // CONSTRUCTEURS
+
+        /* constructeur par défaut */
+
+        public Bouteille()
+        {
+            contenanceMaxEnL = 1.0f;
+            contenuEnL = 1.0f;
+            estOuverte = false;
+        }
+
+        /* constructeur personnalisé */
+
+        public Bouteille(float _contenanceMaxEnL,  float _contenuEnL, bool _estOuverte)
+        {
+            this.contenanceMaxEnL = _contenanceMaxEnL;
+            this.contenuEnL = _contenuEnL;
+            this.estOuverte = _estOuverte;
+        }
+
+        /* constructeur de clonage */
+
+        public Bouteille(Bouteille _bouteilleACopier)
+        {
+            this.contenanceMaxEnL = _bouteilleACopier.contenanceMaxEnL;
+            this.contenuEnL = _bouteilleACopier.contenuEnL;
+            this.estOuverte = _bouteilleACopier.estOuverte;
+        }
+
+
+        // METHODES
+
+        public bool Ouvrir()
+        {
+            bool ouvrirOk;
+            if (this.estOuverte == true)
+            {
+                ouvrirOk = false;
+            }
+            else
+            {
+                this.estOuverte = true;
+                ouvrirOk = true;
+            }
+            return ouvrirOk;
+        }
+
+        public bool Fermer()
+        {
+            bool fermerOk;
+            if (this.estOuverte == false)
+            {
+                fermerOk = false;
+            }
+            else
+            {
+                this.estOuverte = true;
+                fermerOk = true;
+            }
+            return fermerOk;
+        }
+
+
+    }
+}
---
+++ 04 - UML, POO/ApplicationBouteille/ApplicationBouteille/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Metadata;
+
+namespace ApplicationBouteille
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application Bouteille");
+
+            // VERIFICATION DES BOUTEILLES
+
+            Bouteille maBouteille = new Bouteille();
+
+            Bouteille petiteBouteille = new Bouteille(0.5f, 0.5f, false);
+
+            Bouteille petiteBouteilleClonee = new Bouteille(petiteBouteille);
+
+
+            bool okPourOuvrir = petiteBouteille.Ouvrir();
+
+            bool okPourOuvrirBis = petiteBouteille.Ouvrir();
+
+        }
+    }
+}
