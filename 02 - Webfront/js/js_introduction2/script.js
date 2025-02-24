52924bb29a49db1ff7b5e8fde596634ac4f486dd
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-02-24 15:13:34 +0100

    introduction2.js

---
+++ 02 - Webfront/js/js_introduction2/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const paragraphe = document.getElementById('monParagraphe');
+    const boutonAugmenter = document.getElementById('augmenter');
+    const boutonDiminuer = document.getElementById('diminuer');
+    const tailleTexteAffichee = document.getElementById('tailleTexteAffichee');
+    let tailleTexte = 16; // Taille de texte par défaut
+
+    paragraphe.style.fontSize = tailleTexte + 'px';
+    tailleTexteAffichee.textContent = tailleTexte + 'px';
+
+    boutonAugmenter.addEventListener('click', function() {
+        tailleTexte++;
+        if (tailleTexte > 48) {
+            tailleTexte = 16;
+        }
+        paragraphe.style.fontSize = tailleTexte + 'px';
+        tailleTexteAffichee.textContent = tailleTexte + 'px';
+    });
+
+    boutonDiminuer.addEventListener('click', function() {
+        tailleTexte--;
+        if (tailleTexte < 8) {
+            tailleTexte = 16;
+        }
+        paragraphe.style.fontSize = tailleTexte + 'px';
+        tailleTexteAffichee.textContent = tailleTexte + 'px';
+    });
+});
