540c4ddc7ab0875733d86dc1ec2a9ce32298da79
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-02-25 15:35:16 +0100

    date (suite)

---
+++ 02 - Webfront/js/js_dates/assets/script.js
@@ -0,0 +1,10 @@
+const today = Date.now();
+const dob = document.querySelector("#dob");
+const btnCalc = document.querySelector("#calculer");
+const textUpdate = document.querySelector("#text");
+
+btnCalc.addEventListener("click", function() {
+    
+
+});
+
