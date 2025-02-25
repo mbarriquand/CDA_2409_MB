510ca2099b068eef44671d46a6b2e02ae44fdfc6
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-02-25 11:22:51 +0100

    formulaires.js + retraite

--- 02 - Webfront/js/js_formulaires/assets/script.js
+++ 02 - Webfront/js/js_formulaires/assets/script.js
@@ -3,6 +3,8 @@ const btnEmpty = document.querySelector("#empty");
 const inputName = document.querySelector("#name");
 const inputAge = document.querySelector("#age");
 const textUpdate = document.querySelector("#text");
+const ofAgeUpdate = document.querySelector("#ofAge");
+const retraiteUpdate = document.querySelector("#retraite");
 
 /* console.log(btnAdd);
 console.log(inputName); */
@@ -20,10 +22,34 @@ else if (inputAge.value <= 0 || inputAge.value === "") {
 else {
     textUpdate.innerHTML = "Bonjour " + inputName.value +
      ", vous avez " + inputAge.value + " ans.";
+
+    if (inputAge.value < 18) {
+        ofAgeUpdate.innerHTML += " Vous êtes mineur.";
+    }
+
+    else {
+        ofAgeUpdate.innerHTML += " Vous êtes majeur(e).";
+
+        if (inputAge.value >= 65) {
+            retraiteUpdate.innerHTML += " Vous êtes à la retraite depuis" + (inputAge.value - 64) + " ans.";
+        }
+
+        else if (inputAge.value == 64) {
+            retraiteUpdate.innerHTML += " Vous allez prendre votre retraite cette année.";
+        }
+
+        else {
+            retraiteUpdate.innerHTML += " Vous serez à la retraite dans " + (65 - inputAge.value) + " ans.";
+        }
+    }
 }
 
 });
 
 btnEmpty.addEventListener("click", () => {
     textUpdate.innerHTML = "Completer / corriger le formulaire";
+    ofAgeUpdate.innerHTML = "";
+    retraiteUpdate.innerHTML = "";
+    inputName.value = "";
+    inputAge.value = "";
 });
