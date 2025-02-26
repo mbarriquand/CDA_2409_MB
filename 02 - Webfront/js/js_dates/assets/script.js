7ec24a3a751d4fc6fb67243a785a20fcf9a0d87c
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-02-26 12:47:46 +0100

    intro_dates

--- 02 - Webfront/js/js_dates/assets/script.js
+++ 02 - Webfront/js/js_dates/assets/script.js
@@ -1,13 +1,43 @@
-const today =  new Date();
-const btnCalcul = document.querySelector("#calculer");
+let maDate = new Date();
 
+let maChaine = "";
+let annee = maDate.getFullYear();
+// let mois = maDate.getMonth() + 1;
+// let jour = maDate.getDate();
 
-/*
-addEventListener('click', function () {
-    console.log(today); 
+// forcer l'ajout du zéro devant les mois et jours < 10
+// pour avoir une date sur deux jours
+
+let mois = (maDate.getMonth() + 1 < 10)?"0"+(maDate.getMonth() + 1):maDate.getMonth() + 1;
+// exemple de ternaire 
+// ? = if
+// : = else
+
+let jour = (maDate.getDate() < 10)?"0"+maDate.getDate():maDate.getDate();
+
+//maChaine += annee + "-" + mois + "-" + jour;
+maChaine += jour + "/" + mois + "/" + annee;
+
+document.getElementById("btnDate").addEventListener("click", function(){
+    document.getElementById("maDate").value = maChaine;
 });
-*/
 
-btnCalcul.addEventListener('click', function () {
-    console.log(today); 
-});
+
+function afficherHeure(){
+
+    let monHeure = new Date();
+    let heure = (monHeure.getHours() < 10 )?"0"+monHeure.getHours():monHeure.getHours();
+    let minute = (monHeure.getMinutes() < 10 )?"0"+monHeure.getMinutes():monHeure.getMinutes();
+    let seconde = (monHeure.getSeconds() < 10 )?"0"+monHeure.getSeconds():monHeure.getSeconds();
+    
+    let maChaineHeure = heure + ":" + minute + ":" + seconde;
+    document.getElementById("monHeure").value = maChaineHeure;
+    
+    }
+  
+document.getElementById("btnHeure").addEventListener("click",function(){
+
+ const timer = setInterval(afficherHeure, 1000);    
+
+} );
+
