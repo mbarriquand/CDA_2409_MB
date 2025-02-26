8cbb83a6040c846893120d1572fdff4e16ab5958
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-02-26 10:23:05 +0100

    array.js

--- 02 - Webfront/js/js_dates/assets/script.js
+++ 02 - Webfront/js/js_dates/assets/script.js
@@ -1,1 +1,13 @@
+const today =  new Date();
+const btnCalcul = document.querySelector("#calculer");
 
+
+/*
+addEventListener('click', function () {
+    console.log(today); 
+});
+*/
+
+btnCalcul.addEventListener('click', function () {
+    console.log(today); 
+});
