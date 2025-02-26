9970b60e5e5d4cc50c7a8e5a7d243e7576d6f17f
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-02-26 08:17:48 +0100

    date.sjs

--- 02 - Webfront/js/js_dates/assets/script.js
+++ 02 - Webfront/js/js_dates/assets/script.js
@@ -1,10 +1,1 @@
-const today = Date.now();
-const dob = document.querySelector("#dob");
-const btnCalc = document.querySelector("#calculer");
-const textUpdate = document.querySelector("#text");
-
-btnCalc.addEventListener("click", function() {
-    
-
-});
 
