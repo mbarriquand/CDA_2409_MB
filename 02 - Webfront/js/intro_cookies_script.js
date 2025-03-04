8696c7a1254175df4979e2fbbf7187d9327b129f
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-03-04 15:43:16 +0100

    intro_cookies

---
+++ 02 - Webfront/js/intro_cookies_script.js
@@ -0,0 +1,19 @@
+function CreerCookies(nomCookie, valeurCookie){
+
+    let dateJour = new Date();
+    let dateExpire = new Date(dateJour.getFullYear(), dateJour.getMonth(), dateJour.getDate(), (dateJour.getHours()+2));
+
+    dateExpire += dateExpire.toUTCString();
+
+    document.cookie = nomCookie + "=" + valeurCookie + ";expires=" + dateExpire + ";SameSite=Strict";
+}
+
+const btnC = document.querySelector("#btnCookie");
+btnC.addEventListener("click", function(){
+let user = document.querySelector("#username").value;
+
+CreerCookies("Nom", user);
+
+window.location.assign("./intro_cookies_destination.html");
+
+})
