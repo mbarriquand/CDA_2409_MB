67181ee9766442f2795dafebc690c9b0a0eb5640
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-03-12 11:03:59 +0100

    code postaux

--- 02 - Webfront/js/js_codes_postaux/assets/script.js
+++ 02 - Webfront/js/js_codes_postaux/assets/script.js
@@ -1,22 +1,50 @@
-const btn = document.getElementById("#valider");
+fetch("https://arfp.github.io/tp/web/javascript/02-zipcodes/zipcodes.json") // on récupère le json
+  .then((Response) => {
+    return Response.json();
+  })
+  .then(data => {console.log(data);
+    remplirDataList(data);
 
-btn.addEventListener('click', function(){
+    const btnValidation = document.querySelector("#valider");
+    btnValidation.addEventListener('click', function(){
+        const zip = document.querySelector("#cp");
+        let arraySolution = [];
+        arraySolution = data.filter(obj => obj.nomCommune == zip.value || obj.codePostal == zip.value);
 
-async function fetchCP(){
-    try {
-        const cpSearch = document.getElementById("#cp").value;
-    const response = await fetch(`https://arfp.github.io/tp/web/javascript/02-zipcodes/zipcodes.json`);
+        if(arraySolution != "undefined"){
+            //document.querySelector("#result").innerHTML = `Nom de la commune : ${solution.nomCommune}<br>Code postal : ${solution.codePostal}`;
+        document.querySelector("#result").innerHTML = "";
+            arraySolution.forEach(objetVille => {
+                document.querySelector("#result").innerHTML += `Nom de la commune : ${objetVille.nomCommune}<br>Code postal : ${objetVille.codePostal}<br><br>`;
+        });
+        
+    zip.value = "";       
+    zip.focus();
 
-    if(!response.ok){
-        throw new Error("Could not fetch resource");
+        }
+
+        else {
+            document.querySelector("#result").innerHTML = `Commune non trouvée`;
+        }
+    })
+
+
+  })
+  .catch(error => alert("message : " + error));
+
+
+  // création d'une fonction pour afficher les suggestions
+  function remplirDataList(data){
+
+    const datalist = document.getElementById("suggestions");
+
+    for(let i = 0; i < data.length; i++) {
+        
+    const myOption = new Option(); // création d'un constructeur
+    myOption.value = data[i].codeCommune;
+    myOption.textContent = data[i].nomCommune + " cp : " + data[i].codePostal;
+
+    datalist.appendChild(myOption);
     }
 
-    console.log(btn);
-
-
-    }
-catch(error){
-    console.error(error);
-}
-}
-});
+  }
