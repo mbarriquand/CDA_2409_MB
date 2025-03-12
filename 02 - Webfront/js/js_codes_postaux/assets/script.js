5ab8177256fb41532568454d9c30cf57a1497759
mbarriquand <166002061+mbarriquand@users.noreply.github.com>
2025-03-12 11:52:29 +0100

    cp + commentaires

--- 02 - Webfront/js/js_codes_postaux/assets/script.js
+++ 02 - Webfront/js/js_codes_postaux/assets/script.js
@@ -1,50 +1,54 @@
-fetch("https://arfp.github.io/tp/web/javascript/02-zipcodes/zipcodes.json") // on récupère le json
-  .then((Response) => {
-    return Response.json();
+// Récupération du json
+
+fetch("https://arfp.github.io/tp/web/javascript/02-zipcodes/zipcodes.json")
+  // établir la promesse avec le json
+  .then((Reponse) => {
+    return Reponse.json();
   })
-  .then(data => {console.log(data);
+
+  // puis quand la promesse est établie, on fait ça :
+  .then((data) => {
+    // console.log(data);
     remplirDataList(data);
 
     const btnValidation = document.querySelector("#valider");
-    btnValidation.addEventListener('click', function(){
-        const zip = document.querySelector("#cp");
-        let arraySolution = [];
-        arraySolution = data.filter(obj => obj.nomCommune == zip.value || obj.codePostal == zip.value);
+    btnValidation.addEventListener("click", function () {
+      const zip = document.querySelector("#cp");
+      let arraySolution = []; // vider l'affichage
+      arraySolution = data.filter(
+        (obj) => obj.nomCommune == zip.value || obj.codePostal == zip.value
+      );
 
-        if(arraySolution != "undefined"){
-            //document.querySelector("#result").innerHTML = `Nom de la commune : ${solution.nomCommune}<br>Code postal : ${solution.codePostal}`;
+      if (arraySolution != "undefined") {
         document.querySelector("#result").innerHTML = "";
-            arraySolution.forEach(objetVille => {
-                document.querySelector("#result").innerHTML += `Nom de la commune : ${objetVille.nomCommune}<br>Code postal : ${objetVille.codePostal}<br><br>`;
+        arraySolution.forEach((objetVille) => {
+          document.querySelector("#result").innerHTML +=
+          `Nom Commune : ${objetVille.nomCommune}<br>Code Postal : ${objetVille.codePostal}<br> Code Commune : ${objetVille.codeCommune}<br><br>`;
         });
-        
-    zip.value = "";       
-    zip.focus();
 
-        }
+        // vider le champ de saisie
 
-        else {
-            document.querySelector("#result").innerHTML = `Commune non trouvée`;
-        }
-    })
+        zip.value = "";
+        zip.focus();
 
-
+      } else {
+        document.querySelector("#result").innerHTML = `Commune non trouvée`;
+      }
+    });
   })
-  .catch(error => alert("message : " + error));
 
+  // si la promesse échoue, on renvoie un message d'erreur
+  .catch((error) => alert("message : " + error));
 
-  // création d'une fonction pour afficher les suggestions
-  function remplirDataList(data){
+// création d'une fonction pour afficher les suggestions
+function remplirDataList(data) {
+  const datalist = document.getElementById("suggestions");
 
-    const datalist = document.getElementById("suggestions");
-
-    for(let i = 0; i < data.length; i++) {
-        
+  for (let i = 0; i < data.length; i++) {
     const myOption = new Option(); // création d'un constructeur
     myOption.value = data[i].codeCommune;
     myOption.textContent = data[i].nomCommune + " cp : " + data[i].codePostal;
 
     datalist.appendChild(myOption);
-    }
-
   }
+}
